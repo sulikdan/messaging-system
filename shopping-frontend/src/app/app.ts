3165fa7be67885d436fae2bded281d5e75a36e34
sulikdan <sulikdan@outlook.com>
2025-08-31 11:43:14 +0200

    Adding FE styles material + tailwin

--- shopping-frontend/src/app/app.ts
+++ shopping-frontend/src/app/app.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {Header} from './header/header';
+import {Body} from './body/body';
+import {Footer} from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Body, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
